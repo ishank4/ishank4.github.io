821b7f3001978c339c1e45369660b3e495c31f44
ishank4 <ishank4@gmail.com>
2019-07-18 19:29:25 +0530

    updates

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# ishank4.github.io
- 
+# [sindresorhus.com](https://sindresorhus.com)
+
+> Personal website of Sindre Sorhus
+
+*The website targets the latest version of Chrome, Safari, and Firefox.*
+
+<a href="https://sindresorhus.com">
+	<img src="screenshot.jpg" width="600">
+</a>
+
+
+## Dev
+
+```
+$ ./start.sh
+```
+
+
+## License
+
+MIT © [Sindre Sorhus](https://sindresorhus.com)
--- index.js
+++ index.js
@@ -1,185 +1,153 @@
-(function() {
+/* eslint-env browser */
 
-    var width, height, largeHeader, canvas, ctx, points, target, animateHeader = true;
+const isDev = ['localhost', '127.0.0.1'].includes(location.hostname);
 
-    // Main
-    initHeader();
-    initAnimation();
-    addListeners();
+// Helper
+const dom = {
+	select: document.querySelector.bind(document),
+	slectAll: document.querySelectorAll.bind(document)
+};
 
-    function initHeader() {
-        width = window.innerWidth;
-        height = window.innerHeight;
-        target = {x: width/2, y: height/2};
+const injectScript = (source, callback) => {
+	const script = document.createElement('script');
+	script.src = source;
+	script.addEventListener('load', callback);
+	document.head.appendChild(script);
+};
 
-        largeHeader = document.getElementById('large-header');
-        largeHeader.style.height = height+'px';
+const insertHypenationHintsForCamelCase = string => string.replace(/([a-z])([A-Z])/g, '$1\u00AD$2');
 
-        canvas = document.getElementById('demo-canvas');
-        canvas.width = width;
-        canvas.height = height;
-        ctx = canvas.getContext('2d');
+// Latest blog post
+(async () => {
+	const username = 'sindresorhus';
+	const graphql = `
+		query PostQuery($username: String!) {
+			posts(username: $username, limit: 1) {
+				title
+				url
+				subtitle
+			}
+		}
+	`;
 
-        // create points
-        points = [];
-        for(var x = 0; x < width; x = x + width/20) {
-            for(var y = 0; y < height; y = y + height/20) {
-                var px = x + Math.random()*width/20;
-                var py = y + Math.random()*height/20;
-                var p = {x: px, originX: px, y: py, originY: py };
-                points.push(p);
-            }
-        }
+	const url = new URL('https://micro-medium-api-gqmwonovpk.now.sh/graphql');
+	url.searchParams.append('variables', JSON.stringify({username}));
+	url.searchParams.append('query', graphql);
 
-        // for each point find the 5 closest points
-        for(var i = 0; i < points.length; i++) {
-            var closest = [];
-            var p1 = points[i];
-            for(var j = 0; j < points.length; j++) {
-                var p2 = points[j]
-                if(!(p1 == p2)) {
-                    var placed = false;
-                    for(var k = 0; k < 5; k++) {
-                        if(!placed) {
-                            if(closest[k] == undefined) {
-                                closest[k] = p2;
-                                placed = true;
-                            }
-                        }
-                    }
+	const json = await (await fetch(url)).json();
+	const item = json.data.posts[0];
 
-                    for(var k = 0; k < 5; k++) {
-                        if(!placed) {
-                            if(getDistance(p1, p2) < getDistance(p1, closest[k])) {
-                                closest[k] = p2;
-                                placed = true;
-                            }
-                        }
-                    }
-                }
-            }
-            p1.closest = closest;
-        }
+	const title = dom.select('#latest-blog-post a');
+	title.href = item.url;
+	title.textContent = item.title;
 
-        // assign a circle to each point
-        for(var i in points) {
-            var c = new Circle(points[i], 2+Math.random()*2, 'rgba(255,255,255,0.3)');
-            points[i].circle = c;
-        }
-    }
-
-    // Event handling
-    function addListeners() {
-        if(!('ontouchstart' in window)) {
-            window.addEventListener('mousemove', mouseMove);
-        }
-        window.addEventListener('scroll', scrollCheck);
-        window.addEventListener('resize', resize);
-    }
-
-    function mouseMove(e) {
-        var posx = posy = 0;
-        if (e.pageX || e.pageY) {
-            posx = e.pageX;
-            posy = e.pageY;
-        }
-        else if (e.clientX || e.clientY)    {
-            posx = e.clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
-            posy = e.clientY + document.body.scrollTop + document.documentElement.scrollTop;
-        }
-        target.x = posx;
-        target.y = posy;
-    }
-
-    function scrollCheck() {
-        if(document.body.scrollTop > height) animateHeader = false;
-        else animateHeader = true;
-    }
-
-    function resize() {
-        width = window.innerWidth;
-        height = window.innerHeight;
-        largeHeader.style.height = height+'px';
-        canvas.width = width;
-        canvas.height = height;
-    }
-
-    // animation
-    function initAnimation() {
-        animate();
-        for(var i in points) {
-            shiftPoint(points[i]);
-        }
-    }
-
-    function animate() {
-        if(animateHeader) {
-            ctx.clearRect(0,0,width,height);
-            for(var i in points) {
-                // detect points in range
-                if(Math.abs(getDistance(target, points[i])) < 4000) {
-                    points[i].active = 0.3;
-                    points[i].circle.active = 0.6;
-                } else if(Math.abs(getDistance(target, points[i])) < 20000) {
-                    points[i].active = 0.1;
-                    points[i].circle.active = 0.3;
-                } else if(Math.abs(getDistance(target, points[i])) < 40000) {
-                    points[i].active = 0.02;
-                    points[i].circle.active = 0.1;
-                } else {
-                    points[i].active = 0;
-                    points[i].circle.active = 0;
-                }
-
-                drawLines(points[i]);
-                points[i].circle.draw();
-            }
-        }
-        requestAnimationFrame(animate);
-    }
-
-    function shiftPoint(p) {
-        TweenLite.to(p, 1+1*Math.random(), {x:p.originX-50+Math.random()*100,
-            y: p.originY-50+Math.random()*100, ease:Circ.easeInOut,
-            onComplete: function() {
-                shiftPoint(p);
-            }});
-    }
-
-    // Canvas manipulation
-    function drawLines(p) {
-        if(!p.active) return;
-        for(var i in p.closest) {
-            ctx.beginPath();
-            ctx.moveTo(p.x, p.y);
-            ctx.lineTo(p.closest[i].x, p.closest[i].y);
-            ctx.strokeStyle = 'rgba(156,217,249,'+ p.active+')';
-            ctx.stroke();
-        }
-    }
-
-    function Circle(pos,rad,color) {
-        var _this = this;
-
-        // constructor
-        (function() {
-            _this.pos = pos || null;
-            _this.radius = rad || null;
-            _this.color = color || null;
-        })();
-
-        this.draw = function() {
-            if(!_this.active) return;
-            ctx.beginPath();
-            ctx.arc(_this.pos.x, _this.pos.y, _this.radius, 0, 2 * Math.PI, false);
-            ctx.fillStyle = 'rgba(156,217,249,'+ _this.active+')';
-            ctx.fill();
-        };
-    }
-
-    // Util
-    function getDistance(p1, p2) {
-        return Math.pow(p1.x - p2.x, 2) + Math.pow(p1.y - p2.y, 2);
-    }
-    
+	dom.select('#latest-blog-post p').textContent = item.subtitle;
+	dom.select('#latest-blog-post').classList.add('visible');
 })();
+
+// Latest GitHub commit
+(async () => {
+	const username = 'sindresorhus';
+	const email = 'sindresorhus@gmail.com';
+
+	const response = await fetch(`https://api.github.com/users/${username}/events/public`);
+	const json = await response.json();
+
+	// TODO: Support pagination if no suitable event can be found in the first request:
+	// https://developer.github.com/v3/activity/events/#list-public-events-performed-by-a-user
+	let latestCommit;
+	const latestPushEvent = json.find(event => {
+		if (event.type !== 'PushEvent') {
+			return false;
+		}
+
+		// Ensure the commit is authored by me and I'm not just a "committer"
+		latestCommit = event.payload.commits.reverse().find(commit => commit.author.email === email);
+		return Boolean(latestCommit);
+	});
+
+	if (!latestCommit) {
+		dom.select('#latest-commit').textContent = 'No commit';
+		return;
+	}
+
+	const {repo, created_at: createdAt} = latestPushEvent;
+	const repoUrl = `https://github.com/${repo.name}`;
+
+	const commitTitleElement = dom.select('#latest-commit .commit-title');
+	commitTitleElement.href = `${repoUrl}/commit/${latestCommit.sha}`;
+	const commitMessageLines = latestCommit.message.trim().split('\n');
+	commitTitleElement.title = commitMessageLines.slice(1).join('\n').trim();
+	commitTitleElement.textContent = commitMessageLines[0].trim();
+
+	const commitDateElement = dom.select('#latest-commit .commit-date');
+	commitDateElement.textContent = timeago().format(createdAt);
+
+	const repoTitleElement = dom.select('#latest-commit .repo-title');
+	repoTitleElement.href = repoUrl;
+	repoTitleElement.textContent = repo.name.replace(new RegExp(`^${username}/`), '');
+})();
+
+// Latest GitHub repos
+(async () => {
+	const textColorFromBackgroundColor = color => {
+		if (color.length < 5) {
+			color += color.slice(1);
+		}
+
+		return parseInt(color.replace('#', '0x'), 16) > (0xFFFFFF / 2) ? '#333' : '#fff';
+	};
+
+	const url = isDev ? 'http://localhost:3000' : 'https://gh-latest-repos.now.sh';
+
+	const json = await (await fetch(url)).json();
+
+	const template = dom.select('#latest-repos-template');
+	const container = dom.select('#latest-repos');
+
+	for (const repo of json.reverse()) {
+		if (!repo.description) {
+			continue;
+		}
+
+		const content = template.cloneNode(true).content;
+
+		const a = content.querySelector('.latest-repos-title');
+		a.href = repo.url;
+		a.textContent = insertHypenationHintsForCamelCase(repo.name);
+
+		if (repo.primaryLanguage) {
+			const lang = content.querySelector('.latest-repos-language');
+			lang.textContent = repo.primaryLanguage.name;
+			lang.style.color = textColorFromBackgroundColor(repo.primaryLanguage.color);
+			lang.style.backgroundColor = repo.primaryLanguage.color;
+		}
+
+		content.querySelector('.latest-repos-description').textContent = repo.description;
+
+		container.appendChild(document.importNode(content, true));
+	}
+
+	dom.select('#projects').style.opacity = 1;
+})();
+
+// Unicorn easteregg
+dom.select('#unicorn-btn').addEventListener('click', event => {
+	event.preventDefault();
+	injectScript('cornify.js', () => {
+		cornify();
+		cornify();
+		setInterval(cornify, 1000);
+	});
+}, {once: true});
+
+{
+	function hideScrollHint() {
+		if (window.scrollY > 100) {
+			document.removeEventListener('scroll', hideScrollHint);
+			dom.select('#scroll-hint').classList.add('hidden');
+		}
+	}
+
+	document.addEventListener('scroll', hideScrollHint, {passive: true});
+}
